11af6b16e5d3b56046051a1913d2c33eb417618e
nezz <imtaehyun@me.com>
2015-02-26 11:21:59 +0900

    redirect after register

--- js/app.js
+++ js/app.js
@@ -132,10 +132,11 @@ app.controller('BuildnoteController', ['$scope', '$http',
                 headers: {
                     'Content-Type': 'application/json; charset=utf-8'
                 }
-            }).success(function(result) {
-                console.log(result);
+            }).success(function(issue) {
+                location.href = 'http://redmine.ssgadm.com/redmine/issues/' + issue.id;
             }).error(function(result) {
                 console.log(result);
+                alert('레드마인 등록중 문제가 발생했습니다.');
             });
         }
 
